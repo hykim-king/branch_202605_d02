e0f1c9e6170eac3b34fc77f63036e1d5c10b09ce
jyLee <jinyoungjy2323@gmail.com>
2026-05-13 16:45:07 +0900

    feat: Member08.java 파일 신규 생성

---
+++ src/com/pcwk/member/domain08/Member08.java
@@ -0,0 +1,17 @@
+/**
+ * 파일명: Main.java  <br>
+ * 설명:  git branch 실습<br>
+ * 작성자: jinyoung <br>
+ * 작성일: 2026. 5. 13. <br>
+ * 버전 : 1.0
+ */
+package com.pcwk.member.domain08;
+
+public class Member08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
